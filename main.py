a3ab9c95f57d554ea737ab7ba3d69d35bf307f83
MatPugliese92 <mat.pugliese@proton.me>
2026-05-12 12:33:23 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,17 +2,9 @@ import os
 import requests
 
 TOKEN = os.environ["BOT_TOKEN"]
-CHAT_ID = os.environ["CHAT_ID"]
 
-message = "🚀 Astro Flip Bot online e collegato a Telegram!"
+url = f"https://api.telegram.org/bot{TOKEN}/getUpdates"
 
-url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
-
-data = {
-    "chat_id": CHAT_ID,
-    "text": message
-}
-
-response = requests.post(url, data=data)
+response = requests.get(url)
 
 print(response.text)
